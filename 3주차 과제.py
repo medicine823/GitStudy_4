0fc359583e5cba958da4f5cc0003a11d4f4de091
medicine823 <113406392+medicine823@users.noreply.github.com>
2022-11-29 18:52:37 +0900

    Add files via upload

---
+++ 3주차 과제.py
@@ -0,0 +1,27 @@
+# #곱셈
+# a = int(input())
+# b = input()
+
+# aXb2 = a * int(b[2])
+# aXb1 = a * int(b[1])
+# aXb0 = a * int(b[0])
+# aXb = a * int(b)
+
+# print(aXb2, aXb1, aXb0, aXb, sep='\n')
+
+
+
+
+
+#ACM 호텔
+# t = int(input())
+
+# for i in range(t):
+#     h, w, n = map(int, input() .split())
+#     num = n//h+1
+#     floor = n % h
+#     if n % h == 0:
+#         num = n//h
+#         floor = h
+
+#     print(f'{floor*100+num}')
